b783a3619b3ebd129cee7362440eb99d7379eb42
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2022-12-17 01:35:16 +0530

    Update Country.java

--- src/main/java/com/eaiesb/demo/Country.java
+++ src/main/java/com/eaiesb/demo/Country.java
@@ -3,6 +3,9 @@ package com.eaiesb.demo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
+@Data
 @Document(collection="Country")
 public class Country {
 	@Id
@@ -17,38 +20,6 @@ public class Country {
 		this.Country_name= Country_name;
 		this.Region_id= Region_id;
 		}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getCountry_id() {
-		return Country_id;
-	}
-
-	public void setCountry_id(String country_id) {
-		Country_id = country_id;
-	}
-
-	public String getCountry_name() {
-		return Country_name;
-	}
-
-	public void setCountry_name(String country_name) {
-		Country_name = country_name;
-	}
-
-	public String getRegion_id() {
-		return Region_id;
-	}
-
-	public void setRegion_id(String region_id) {
-		Region_id = region_id;
-	}
 	
 
 }
